c5e975f046aa048e08e38e89f0586e9a50167592
davidpardoC <pardodavid10@gmail.com>
2020-12-27 17:54:09 -0500

    BUrned initial routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
-const express = require('express')
-const app = express()
-const http = require('http').createServer(app);
-const io = require('socket.io')(http);
-console.log('Hola mundo');
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+const port = 8000;
+var cors = require('cors')
+app.use(cors())
+
+app.get("/get-subjects", (req, res) => {
+  res.json({
+    data: {
+      subjects: [
+        { id: 1, name: "Ingenieria de software y desarrollo" },
+        { id: 2, name: "Ingenieria de software y gestión ti" },
+        { id: 3, name: "Laboratorio de datos" },
+        { id: 4, name: "UTPL_PROYECTOS_LAB" },
+      ],
+    },
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
